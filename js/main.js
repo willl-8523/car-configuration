2f52f46e91eea928365a230ad81237295215472a
willl85 <ngowilfried@gmail.com>
2024-11-18 19:40:41 +0100

    Handle Color Selection

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-const topBar = document.querySelector('#top-bar')
+const topBar = document.querySelector('#top-bar');
+const exteriorColorSection = document.querySelector('#exterior-buttons');
+const interiorColorSection = document.querySelector('#interior-buttons');
+const exteriorImage = document.querySelector('#exterior-image');
+const interiorImage = document.querySelector('#interior-image');
 
 // Handle Top Bar On Scroll
 const handleScroll = () => {
@@ -6,12 +10,32 @@ const handleScroll = () => {
 
   /**
    * element.classList.toggle('class-name', condition)
-   * => Add class-name if condition is true 
+   * => Add class-name if condition is true
    * => Remove class-name if condition is false
    */
   topBar.classList.toggle('visible-bar', atTop);
   topBar.classList.toggle('hidden-bar', !atTop);
-}
+};
+
+// Handle Color Selection
+const handleColorButtonClick = (event) => {
+  let button;
+
+  if (event.target.tagName === 'IMG') {
+    button = event.target.closest('button');
+  } else if (event.target.tagName === 'BUTTON') {
+    button = event.target;
+  }
+
+  if (button) {
+    const buttons = event.currentTarget.querySelectorAll('button');
+
+    buttons.forEach((btn) => btn.classList.remove('btn-selected'));
+    button.classList.add('btn-selected');
+  }
+};
 
 // Event Listeners
-window.addEventListener('scroll', () => requestAnimationFrame(handleScroll))
+window.addEventListener('scroll', () => requestAnimationFrame(handleScroll));
+exteriorColorSection.addEventListener('click', handleColorButtonClick);
+interiorColorSection.addEventListener('click', handleColorButtonClick);
